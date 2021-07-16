94446f07d316128a4e3e7485d767e50c8d2d9350
brindakarthik <79500601+brindakarthik@users.noreply.github.com>
2021-07-16 05:59:59 +0530

    add border in css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const customStyle = {
+  color: "red",
+  fontSize: "12px",
+  border: "1px solid black"
+};
 ReactDOM.render(
-  <h1 style={{ color: "red" }}>Hello World!</h1>,
+  <h1 style={customStyle}>Hello World!</h1>,
   document.getElementById("root")
 );
